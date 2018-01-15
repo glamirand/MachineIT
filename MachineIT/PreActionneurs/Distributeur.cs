0fe8441cc6f118e31a51826b237ae4ef7cf3e89e
Gaël LAMIRAND <perso@gael-lamirand.com>
2018-01-15 12:54:39 +0100

    Original push

---
+++ MachineIT/PreActionneurs/Distributeur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineIT.PreActionneurs.Verins
+{
+    class Distributeur : PreActionneur
+    {
+    }
+}
